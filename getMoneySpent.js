6521281c474ab9a809084530a50ae304edfd292f
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-05 21:44:49 +0530

    Created to return the maximum total price for the two items within Monica's budget

--- getMoneySpent.js
+++ getMoneySpent.js
@@ -1,49 +1,34 @@
-let drives = [];
-let keyboards = [];
-let driveArrLength = drives.length;
-let keyboardsArrLength = keyboards.length;
-const sumOfItemPriceArr = [];
-let sumOfPrice;
-
 function getMoneySpent(keyboards, drives, b) {
+
+  const sumOfItemPriceArr = [];
+
   keyboards.forEach((element) => {
     //console.log(element);
 
     drives.forEach((usb) => {
       //console.log(usb);
-
-      let sumOfPrice = element + usb;
+      let sumOfPrice;
+      sumOfPrice = element + usb;
       //console.log(sumOfPrice);
+
       sumOfItemPriceArr.push(sumOfPrice);
+
     });
+
   });
+
   //console.log(sumOfItemPriceArr);
   
   let maxItemPrice = Math.max(...sumOfItemPriceArr);
-  const map = new Map;
-  map.set(maxItemPrice,b);
-  console.log(map);
-  // for (let [key, value] of  map.entries()) {
-  // console.log(key + " = " + value)
-  // if(key < value){
-  //   console.log(key);
-  //   //return key;
-  // }
-  // else {
-  //   return -1;
-  // }
-//}
-  // //console.log(maxItemPrice);
-  //  if(maxItemPrice < b){
-  //     return maxItemPrice;
-  //     }
-  //     else{
-  //       return -1;
-  //   } 
+  
+    if(maxItemPrice < b){
+      return maxItemPrice;
+      }
+      else{
+        return -1;
+    } 
 }
-//console.log(getMoneySpent([3, 1], [5, 2, 8], 10));
-// console.log(getMoneySpent([1, 2], [7, 6], 10));
-// console.log(getMoneySpent([4], [5], 5));
-getMoneySpent([1, 2], [7, 6], 10)
-getMoneySpent([4], [5], 5);
-getMoneySpent([3, 1], [5, 2, 8], 10)
+console.log(getMoneySpent([3, 1], [5, 2, 8], 10));
+console.log(getMoneySpent([1, 2], [7, 6], 10));
+console.log(getMoneySpent([4], [5], 5));
+

7e74b062a0196b756a091e83d66169a554e24fd7
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-13 10:35:25 +0530

    console.log user the winner of the game after comparing the AI and user value

--- chooseWinner.js
+++ chooseWinner.js
@@ -1,66 +1,29 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
-function chooseWinner(gameCount){
+function chooseWinner(event) {
+  let userInput = prompt("Enter the dice number:");
+  //console.log(userInput);
+  var userValue = parseInt(userInput);
+  // console.log("userValue", userValue);
+  let randomNum = Math.floor(Math.random() * (6 - 0) + 0);
+  // console.log("randomNum", randomNum);
+  let AIMarks = 0;
+  let userMarks = 0;
 
-	let computerWinnerMarks;	
-	let userWinnerMarks;
-const map = new Map;
-
-for (let i=1; i<=gameCount; i++){
-	let userInput = prompt("Enter the dice number:");
-	//console.log(userInput);
-
-	var userValue = parseInt(userInput);
-
-	let randomNum =  Math.floor(Math.random() * (6 - 0) + 0);
-	//console.log(randomNum);
-
-	// if(userValue < randomNum){
-	//  computerWinnerMarks += 6;
-
-	// }
-	// else if(userValue == randomNum){
-	// 	 userWinnerMarks += 3;
-	// 	 computerWinnerMarks += 3;
-
-	// }
-	// else{
-	// 	 userWinnerMarks += 6;
-	// }
-
-	map.set(userValue,randomNum);
-
+  //loop until keypress
+  if (userValue <= randomNum) {
+      AIMarks = AIMarks + 2;
+      console.log("Try again!, AI is the Winner");
+      console.log("Score:", AIMarks);
+    } else if (userValue == randomNum) {
+      console.log("Game is draw!");
+    } else if (userValue >= randomNum) {
+      userMarks = userMarks + 2;
+      console.log("Congrats!, You are the Winner");
+      console.log("Score:", userMarks);
+    } else {
+      console.log("Input is not valid!");
+    }
 }
 
-
-//console.log(map.size);
-
-for (let x=0; x<map.size; x++){
-	if(map.keys() < map.values()){
-		computerWinnerMarks = computerWinnerMarks + 2;
-		//console.log(computerWinnerMarks);
-
-	}
-
-	else if (map.keys() == map.values()){
-		computerWinnerMarks = computerWinnerMarks + 1;
-		userWinnerMarks = userWinnerMarks + 1;
-
-	}
-
-	else{
-		userWinnerMarks = userWinnerMarks + 2;
-	}
-}
-	
-// console.log(userWinnerMarks);
-// console.log(computerWinnerMarks);
-
-
-
-
-	
-}
-
-
-chooseWinner(2);
+chooseWinner();
